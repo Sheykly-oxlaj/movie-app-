b40e06b771dc8f8f19db49f7d02c4b4234bf08a7
Sheykly Oxlaj <oxlajsheylt@gmail.com>
2022-07-06 15:20:46 -0700

    adds RESTful routes for the Movie model

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,21 @@ Rails.application.routes.draw do
   # DELETE "/actors/3" - Deletes a specific actor
   delete "/actors/:id" => "actors#destroy"
 
+#############################################################
+
+   # GET "/movies" - Gets all the movies we have
+   get "/movies" => "movies#index"
+
+   # GET "/movies/3" - Just get one specific actor (that already exists)
+   get "/movies/:id" => "movies#show"
+ 
+   # POST "/movies" - Creates a new actor, add it to our database
+   post "/movies" => "movies#create"
+ 
+   # PATCH "/movies/3" - Updates a specific actor
+   patch "/movies/:id" => "movies#update"
+ 
+   # DELETE "/movies/3" - Deletes a specific actor
+   delete "/movies/:id" => "movies#destroy"
 end
 
