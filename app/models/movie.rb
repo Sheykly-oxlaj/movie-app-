6e04a7327f3153b3adc073c6a77e5784fc1f3ad2
Sheykly Oxlaj <oxlajsheylt@gmail.com>
2022-07-18 18:08:08 -0700

    fixes associations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,7 @@
 class Movie < ApplicationRecord
-  has_many :actors
-  has_many :genres
   has_many :movie_genres
   has_many :genres, through: :movie_genres
+  has_many :actors
   validates :title, presence: true
   validates :year, presence: true
   validates :plot, length: { minimum: 10 }
