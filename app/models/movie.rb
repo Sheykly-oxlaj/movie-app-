0199209b48ce01ba02e0ea89e3127f798e670c7c
Sheykly Oxlaj <oxlajsheylt@gmail.com>
2022-07-18 19:48:26 -0700

    attempts to add genre_names method

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,4 +6,11 @@ class Movie < ApplicationRecord
   validates :year, presence: true
   validates :plot, length: { minimum: 10 }
   validates :director, presence: true
+
+  def genre_names
+    genres = MovieGenre.where(movie_id: id)
+    # # Genre.where(name: )
+    # genres = Genre.where(name: movie.genres)
+    genres.id
+  end
 end
